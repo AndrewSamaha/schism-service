83884ae4f25c38519edd89cb86d04725358a7945
Andrew Samaha <andrew.samaha@gmail.com>
2022-06-12 08:33:56 -0400

    WIP: knex seeding

--- src/db/deploy.js
+++ src/db/deploy.js
@@ -4,8 +4,8 @@ const deploy = async (knex) => {
     console.log('deploying database schema');
     await modelCollection.sort((a,b) => a.PRIORITY - b.PRIORITY).map(async x => {
         console.log('deploying', x.TABLENAME, 'priority: ', x.PRIORITY);
-        await x.createSchema(knex);
-        if (x.seed) await x.seed(knex);
+        const created = await x.createSchema(knex);
+        if (created && x.seed) await x.seed(knex);
     });
     console.log('finished deploying schema');
 }
--- src/models/Terrain.js
+++ src/models/Terrain.js
@@ -6,44 +6,69 @@ const PRIORITY = 100;
 
 class Terrain extends Model {
     static get tableName() { return TABLENAME; }
+    static get relationMapping() {
+        const TileType = require('./TileType.js');
+        return {
+            TileType: {
+                relation: Model.HasOneRelation,
+                modelClass: TileType,
+                join: {
+                    from: `${TABLENAME}.tileTypeId`,
+                    to: `${TileType.TABLENAME}.id`
+                }
+            }
+        }
+    }
 }
 
 async function createSchema(knex) {
     console.log(`createSchema ${TABLENAME}`);
     if (await knex.schema.hasTable(TABLENAME)) {
         console.log(`createSchema ${TABLENAME} failed/ table exists`)
-        return;
+        return false;
     }
 
     await knex.schema.createTable(TABLENAME, table => {
         table.increments('id').primary();
-        table.integer('tileTypeId');
+        table.integer('tileTypeId');//.references('id').inTable('TileTypes');
         table.integer('x');
         table.integer('y');
-        table.integer('z');
+        // table.integer('z');
     });
 
     console.log(`created ${TABLENAME}`);
+    return true;
 }
 
-// async function seed(knex) {
-//     Model.knex(knex);
-//     const types = [
-//         'grass',
-//         'stone',
-//         'water',
-//         'sand'
-//     ];
-//     types.map((type) => {
-//         await TileType.query().insert({type})
-//     });
-//     const verifiedTypes = await TileType.query().orderBy('id');
-//     console.log({verifiedTypes});
-// }
+async function seed(knex) {
+    Model.knex(knex);
+    const { TileType } = require('./TileType.js');
+    const tileTypes = await TileType.query().orderBy('id');
+    const rowSize = 2;
+    const rows = 2;
+    const startPos = { x: 0, y: 0 };
+    for (let x = startPos.x; x < startPos.x + rowSize; x++) {
+        for (let y = startPos.y; y < startPos.y + rows; y++) {
+            await Terrain.query().insertGraph({
+                x,
+                y,
+                tileTypeId: 1
+            });
+        }
+    }
+    const verifiedTerrain = await Terrain
+        .query()
+        .joinRelated('TileType')
+        .where('TileType.id','tileTypeid')
+        ; // .orderBy('id');
+    // const verifiedTerrain = await Terrain.query().orderBy('id');
+    console.log({verifiedTerrain});
+}
 
 module.exports = {
     TABLENAME,
     Terrain,
     createSchema,
-    PRIORITY
+    PRIORITY,
+    seed
 }
